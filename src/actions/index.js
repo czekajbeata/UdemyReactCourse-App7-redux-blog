c0c1b568d7035b101bc062c33268d7d36c4e4154
Beata <beata.czekaj.7@gmail.com>
2021-02-11 18:56:55 +0100

    added joined action creator to fetch posts and users

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -15,3 +23,15 @@ export const fetchUser = (userId) => async (dispatch) => {
     payload: response.data,
   });
 };
+
+// lodash memoized action creator - single call for each passed parameter during entire life cycle
+
+// export const fetchUser = (userId) => (dispatch) => _fetchUser(userId, dispatch);
+
+// const _fetchUser = _.memoize(async (userId, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${userId}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
